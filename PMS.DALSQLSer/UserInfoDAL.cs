8ca56bd4006b1f6e1b2abbe0a8b3485cae8aee6b
liusihan <evaseemefly@126.com>
2016-03-13 15:16:30 +0800

    修改DAL以及IDAL层，新建BLL层以及IBLL层——家

--- PMS.DALSQLSer/UserInfoDAL.cs
+++ PMS.DALSQLSer/UserInfoDAL.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 using PMS.Model;
 using System.Data.Entity.Infrastructure;
 using System.Linq.Expressions;
+using PMS.IDAL;
 
 namespace PMS.DALSQLSer
 {
-    public class UserInfoDAL
+    /// <summary>
+    /// 用户数据访问层继承自IUserInfo接口
+    /// </summary>
+    public class UserInfoDAL: IUserInfo
     {
         //1 创建实体上下文对象，并将继承自DbContext的EF实体对象PMS2016Entities赋给实体上下文对象
         DbContext Db = new PMS2016Entities();
@@ -21,13 +25,9 @@ namespace PMS.DALSQLSer
          增删改查
          */
 
-       //public void Test()
-       // {
-       //     this.Add()
-       // }
-        
+        #region 1 根据实体为数据库中添加新的对象+public bool Create(UserInfo model)
         /// <summary>
-        /// 根据实体为数据库中添加新的对象
+        /// 1 根据实体为数据库中添加新的对象
         /// </summary>
         /// <param name="model">UserInfo实体对象</param>
         /// <returns></returns>
@@ -37,16 +37,29 @@ namespace PMS.DALSQLSer
             Db.Set<UserInfo>().Add(model);
             return true;
         }
+        #endregion
 
-        //从数据库中删除某个实体
+        #region 2 从数据库中删除某个实体+public bool Del(UserInfo model)
+        /// <summary>
+        /// 2 从数据库中删除某个实体
+        /// </summary>
+        /// <param name="model">删除的实体对象</param>
+        /// <returns></returns>
         public bool Del(UserInfo model)
         {
-            //去掉Attach尝试一下
+            //1 将实体对象添加到上下文中
             Db.Set<UserInfo>().Attach(model);
-            //
+            //2 将该实体对象标记为删除
             Db.Set<UserInfo>().Remove(model);
             return true;
         }
+        #endregion
+
+        #region 对于委托的示例
+        //public void Test()
+        // {
+        //     this.Add()
+        // }
 
         //public delegate void Dg_Test(string str);
 
@@ -60,7 +73,14 @@ namespace PMS.DALSQLSer
         //    dg("我是通过委托传递的参数");
         //    //Test("");
         //}
+        #endregion
 
+        #region 3 修改单独一个实体对象+public bool Update(UserInfo model)
+        /// <summary>
+        /// 3 修改单独一个实体对象
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         public bool Update(UserInfo model)
         {
             //Dg_Test dg_test = new Dg_Test(Test);
@@ -70,32 +90,107 @@ namespace PMS.DALSQLSer
             //1 将UserInfo对象 加入 EF 容器中，并获取实体对象的管理状态
             DbEntityEntry<UserInfo> entry = Db.Entry<UserInfo>(model);
             //2 设置该对象为修改过的状态
-            entry.State = System.Data.EntityState.Unchanged;
+            entry.State = System.Data.EntityState.Modified;
 
             return true;
 
         }
+        #endregion
 
+        #region 3 批量修改实体对象+public bool UpdateByList(List<UserInfo> list)
         /// <summary>
-        /// 查询用户信息
+        ///  3 批量修改实体对象
         /// </summary>
-        /// <param name="whereLambda"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        public bool UpdateByList(List<UserInfo> list)
+        {
+            //遍历传入的要修改的集合，将每个对象的状态均设置为修改状态
+            foreach (var item in list)
+            {
+                DbEntityEntry<UserInfo> entry = Db.Entry<UserInfo>(item);
+                entry.State = System.Data.EntityState.Modified;
+            }
+            return true;
+        }
+        #endregion
+
+        #region 4 查询用户信息+ public IQueryable<UserInfo> GetListBy
+        /// <summary>
+        /// 4 查询用户信息
+        /// </summary>
+        /// <param name="whereLambda">查询条件（lambda）</param>
         /// <returns></returns>
         public IQueryable<UserInfo> GetListBy(Expression<Func<UserInfo, bool>> whereLambda)
         {
             //Db.Set<int>().Where()
             return Db.Set<UserInfo>().Where(whereLambda);
         }
+        #endregion
 
-        public IQueryable<UserInfo> GetListBy<Tkey>(Expression<Func<UserInfo, bool>> whereLambda,Expression<Func<UserInfo,Tkey>>orderLambda)
+        #region 4 根据条件 排序并查询+IQueryable<UserInfo> GetListBy<Tkey>
+        /// <summary>
+        ///  4 根据条件 排序并查询
+        /// </summary>
+        /// <typeparam name="Tkey"></typeparam>
+        /// <param name="whereLambda"></param>
+        /// <param name="orderLambda"></param>
+        /// <returns></returns>
+        public IQueryable<UserInfo> GetListBy<Tkey>(Expression<Func<UserInfo, bool>> whereLambda, Expression<Func<UserInfo, Tkey>> orderLambda)
         {
             return Db.Set<UserInfo>().Where(whereLambda).OrderBy(orderLambda);
         }
+        #endregion
 
+        #region 5 分页查询+public IQueryable<UserInfo> GetPageList<TKey>
+        /// <summary>
+        /// 5 分页查询
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="pageIndex">第几页</param>
+        /// <param name="pageSize">页容量</param>
+        /// <param name="whereLambda">查询条件</param>
+        /// <param name="orderByLambda">排序条件</param>
+        /// <param name="isAsc">是否为升序，true为升序</param>
+        /// <returns>查询结果序列</returns>
+        public IQueryable<UserInfo> GetPageList<TKey>(int pageIndex, int pageSize, Expression<Func<UserInfo, bool>> whereLambda, Expression<Func<UserInfo, TKey>> orderByLambda, bool isAsc)
+        {
+            //5.1 根据查询条件(whereLambda)返回查询 序列
+            var query = Db.Set<UserInfo>().Where(whereLambda);
+
+            //5.2 根据升降序标记判断升序降序排列
+            if (isAsc)
+            {
+                //根据键按升序排序
+                query = query.OrderBy(orderByLambda);
+            }
+            else
+            {
+                //根据键按降序排列
+                query = query.OrderByDescending(orderByLambda);
+            }
+
+            /*5.3
+                .Skip 跳过一定数量的元素，返回剩余元素
+                .Take 从序列的开头返回指定数量的连续元素
+             */
+            return query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+        }
+        #endregion
+
+        #region 6 对EF上下文对象保存所有修改并提交至数据库+public bool SaveChange()
+        /// <summary>
+        /// 6 对EF上下文对象保存所有修改并提交至数据库
+        /// </summary>
+        /// <returns></returns>
         public bool SaveChange()
         {
-            return Db.SaveChanges()>0;
+            return Db.SaveChanges() > 0;
         }
+        #endregion
+
+
 
     }
 }
---
+++ PMS.IDAL/IUserInfo.cs
@@ -0,0 +1,98 @@
+﻿using PMS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.IDAL
+{
+    /// <summary>
+    /// 定义UserInfo实现类的接口
+    /// 注意：
+    /// 1 接口必须是是公开的，因为需要由实现类去继承（实现）
+    /// 2 接口中的方法不需要添加访问修饰符（public），且没有方法体，只有方法签名
+    /// </summary>
+    public interface IUserInfo
+    {
+        #region 1 根据实体为数据库中添加新的对象+bool Create(UserInfo model);
+        /// <summary>
+        /// 1 根据实体为数据库中添加新的对象
+        /// </summary>
+        /// <param name="model">UserInfo实体对象</param>
+        /// <returns></returns>
+        bool Create(UserInfo model);
+        #endregion
+
+        #region 2 从数据库中删除某个实体 +bool Del(UserInfo model);
+        /// <summary>
+        /// 2 从数据库中删除某个实体
+        /// </summary>
+        /// <param name="model">删除的实体对象</param>
+        /// <returns></returns>
+        bool Del(UserInfo model);
+        #endregion
+
+        #region 3 修改单独一个实体对象+ bool Update(UserInfo model);
+        /// <summary>
+        /// 3 修改单独一个实体对象
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        bool Update(UserInfo model);
+        #endregion
+
+        #region 3 批量修改实体对象+ bool UpdateByList(List<UserInfo> list);
+        /// <summary>
+        ///  3 批量修改实体对象
+        /// </summary>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        bool UpdateByList(List<UserInfo> list);
+        #endregion
+
+        #region 4 查询用户信息+IQueryable<UserInfo> GetListBy
+        /// <summary>
+        /// 4 查询用户信息
+        /// </summary>
+        /// <param name="whereLambda">查询条件（lambda）</param>
+        /// <returns></returns>
+        IQueryable<UserInfo> GetListBy(Expression<Func<UserInfo, bool>> whereLambda);
+        #endregion
+
+        #region 4 根据条件 排序并查询+IQueryable<UserInfo> GetListBy<Tkey>
+        /// <summary>
+        ///  4 根据条件 排序并查询
+        /// </summary>
+        /// <typeparam name="Tkey"></typeparam>
+        /// <param name="whereLambda"></param>
+        /// <param name="orderLambda"></param>
+        /// <returns></returns>
+        IQueryable<UserInfo> GetListBy<Tkey>(Expression<Func<UserInfo, bool>> whereLambda, Expression<Func<UserInfo, Tkey>> orderLambda);
+        #endregion
+
+        #region 5 分页查询+IQueryable<UserInfo> GetPageList<TKey>
+        /// <summary>
+        /// 5 分页查询
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <param name="pageIndex">第几页</param>
+        /// <param name="pageSize">页容量</param>
+        /// <param name="whereLambda">查询条件</param>
+        /// <param name="orderByLambda">排序条件</param>
+        /// <param name="isAsc">是否为升序，true为升序</param>
+        /// <returns>查询结果序列</returns>
+        IQueryable<UserInfo> GetPageList<TKey>(int pageIndex, int pageSize, Expression<Func<UserInfo, bool>> whereLambda, Expression<Func<UserInfo, TKey>> orderByLambda, bool isAsc);
+        #endregion
+
+        #region 保存EF上下文对象的修改+bool SaveChange();
+        /// <summary>
+        /// 保存EF上下文对象的修改
+        /// </summary>
+        /// <returns></returns>
+        bool SaveChange();
+        #endregion
+
+    }
+}
