a7b00f0d30b556a0f80c46869ff2ec120056d253
liusihan <evaseemefly@126.com>
2016-03-21 16:11:30 +0800

    1 创建展示验证码 在SMSOA.Login中添加LoginController 控制器，添加ValidateCode（展示验证码并将验证码中的字符串保存至Session中）

--- SMSOA.Login/LoginController.cs
+++ SMSOA.Login/LoginController.cs
@@ -12,5 +12,19 @@ namespace SMSOA.Login
         {
             return View();
         }
+
+        /// <summary>
+        /// 展示验证码并将验证码中的字符串保存至Session中
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult ValidateCode()
+        {
+            Common.ValidateCode validateCode = new Common.ValidateCode();
+            string code = validateCode.CreateValidateCode(4);//创建长度为4个字母的图片
+            Session["validateCode"] = code; //将验证码保存至Session（未保存至分布式缓存）
+            byte[] buffer = validateCode.CreateValidateGraphic(code);
+            return File(buffer, "imge.jepg");
+
+        }
     }
 }
