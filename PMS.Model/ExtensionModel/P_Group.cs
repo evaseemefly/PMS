23c847e537e64580f4624874ca30b02d1fc9724c
liusihan-单位 <evaseemefly@126.com>
2016-04-18 17:17:03 +0800

    4月18日-完成group模块

---
+++ PMS.Model/ExtensionModel/P_Group.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Model
+{
+   public partial class P_Group
+    {
+        /// <summary>
+        /// 将P_Group集合转换为EasyUICombobox集合
+        /// </summary>
+        /// <param name="list_group"></param>
+        /// <returns></returns>
+        public static List<EasyUIModel.EasyUICombobox> ToEasyUICombobox(List<P_Group> list_group )
+        {
+            List<EasyUIModel.EasyUICombobox> list_combox = new List<EasyUIModel.EasyUICombobox>();
+            foreach (var item in list_group)
+            {
+                EasyUIModel.EasyUICombobox combobox = new EasyUIModel.EasyUICombobox()
+                {
+                    id = item.GID,
+                    remark = item.Remark,
+                    text = item.GroupName
+                };
+                list_combox.Add(combobox);
+
+            }
+            return list_combox;
+        }
+    }
+}
