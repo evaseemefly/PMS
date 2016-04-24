637bb241335b9f84e76a096a97269fc439e0084c
t430 <evaseemefly@126.com>
2016-04-24 16:52:10 +0800

    4月24日 1 权限管理模块 将为角色赋予权限显示界面改为非分页视图 2 手风琴及里面的tree实现动态加载 3 通讯模块中部门及群组完成

--- SMSOA/Areas/Admin/Controllers/HomeController.cs
+++ SMSOA/Areas/Admin/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace SMSOA.Areas.Admin.Controllers
     {
         IUserInfoBLL userInfoBLL { get; set; }
 
-        [LoginValidate]
+        //[LoginValidate]
         // GET: Admin/Home
         public ActionResult Index()
         {
@@ -25,6 +25,61 @@ namespace SMSOA.Areas.Admin.Controllers
         public ActionResult NotFound()
         {
             return View();
+        }
+
+        public /*JsonResult*/ActionResult GetMenuFatherNode()
+        {
+            int parentId = 0;
+            int userId = 3;
+
+            //1 从数据库中读取指定的用户对象
+            var userInfo = userInfoBLL.GetListBy(u => u.ID == userId).FirstOrDefault();
+            if (userInfo != null)
+            {
+
+
+                //2 通过路线二查询 UserInfo所对应的角色，并查询该角色中包含的Action
+                var list_action = (
+                    from r in userInfo.RoleInfo //linq
+                    from a in r.ActionInfo
+                    select a).ToList();
+
+                //3 获取该用户对象对应的R_UserInfo_ActionInfo导航属性集合
+                //list_R_User_Action存储的是userInfoId为1的R_User_Action对象的集合
+                var list_R_User_Action = userInfo.R_UserInfo_ActionInfo;
+                //4 取出userInfo id为2的用户所对应的Action集合（路线一的方式）
+
+                //4.1 取出isPass为true的所有集合
+                var list_action_isPass = (
+                   from r in list_R_User_Action
+                   where r.isPass == true
+                   select r.ActionInfo
+                    ).ToList();
+
+                //4.2 将路线一与路线二取出的ActionInfo集合合并
+                list_action.AddRange(list_action_isPass);
+                //4.3 此时的集合中可能存在重复，去重
+                list_action = list_action.Distinct(new PMS.Model.EqualCompare.ActionEqualCompare()).ToList();
+
+                //4.4 取出isPass为false的集合
+                var list_action_isNotPass = (
+                  from r in list_R_User_Action
+                  where r.isPass == false
+                  select r.ActionInfo
+                   ).ToList();
+
+                //4.5 将现有集合中去掉isPass为false的ActionInfo
+                list_action = list_action.Where(a => !list_action_isNotPass.Contains(a)).ToList();
+
+                var list_final = list_action.Where(a => a.ParentID == 0).ToList();
+                List<PMS.Model.EasyUIModel.EasyUITreeNode> list_tree = new List<PMS.Model.EasyUIModel.EasyUITreeNode>();
+                list_final.ForEach(a => list_tree.Add(new PMS.Model.EasyUIModel.EasyUITreeNode() { id = a.ID, text = a.ActionInfoName }));
+                return Content(Common.SerializerHelper.SerializerToString(list_tree));
+                //return Json(list_tree);
+                
+            }
+            return Content("0");
+            //return Json("0", JsonRequestBehavior.AllowGet);
         }
 
         //[LoginValidate]
