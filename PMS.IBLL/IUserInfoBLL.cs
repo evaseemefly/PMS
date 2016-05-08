25c3786e6dd0e73caf7b816fda96fe32dc741dfb
t430 <evaseemefly@126.com>
2016-05-08 11:41:15 +0800

    5月8日 完成常用群组及任务的设置

--- PMS.IBLL/IUserInfoBLL.cs
+++ PMS.IBLL/IUserInfoBLL.cs
@@ -19,6 +19,23 @@ namespace PMS.IBLL
         List<S_SMSMission> GetMissionListByUID(int uid, bool isMiddle);
 
         /// <summary>
+        /// 根据传入的用户id获取该用户的常用群组，并从常用群组中删除传入的群组id集合
+        /// </summary>
+        /// <param name="list_ids_group">已经拥有的群组id（需要从常用群组中剔除的群组id集合）</param>
+        /// <param name="uid">用户id</param>
+        /// <param name="isMiddel">是否转成中间对象</param>
+        /// <returns></returns>
+        List<P_Group> GetRestGroupListByIds(List<int> list_ids_group, int uid, bool isMiddel);
+
+        /// <summary>
+        /// 根据用户Id查询该用户所拥有的群组集合
+        /// </summary>
+        /// <param name="uid">用户Id</param>
+        /// <param name="isMiddle">是否中间转换</param>
+        /// <returns></returns>
+        List<P_Group> GetGroupListByUID(int uid, bool isMiddle);
+
+        /// <summary>
         /// 逻辑删除（物理删除）
         /// </summary>
         /// <param name="list"></param>
@@ -31,6 +48,22 @@ namespace PMS.IBLL
         /// <param name="list_ids"></param>
         /// <returns></returns>
         bool DelSoftUserInfos(List<int> list_ids);
+
+        /// <summary>
+        /// 为指定User重新赋予新的群组——指定用户的常用群组（删除不在此任务id集合中的现有群组关系）
+        /// </summary>
+        /// <param name="userID">用户id</param>
+        /// <param name="list_groupIDs">现拥有的群组id集合</param>
+        /// <returns></returns>
+        bool SetUser4Group(int userID, List<int> list_groupIDs);
+
+        /// <summary>
+        /// 为指定User重新赋予新的任务（删除不在此任务id集合中的现有任务关系）
+        /// </summary>
+        /// <param name="userID">用户id</param>
+        /// <param name="list_missionIDs">拥有的任务id集合</param>
+        /// <returns></returns>
+        bool SetUser4Mission(int userID, List<int> list_missionIDs);
 
         /// <summary>
         /// 为用户分配角色
---
+++ PMS.Model/EasyUIModel/EasyUIDataGrid_Mission.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Model.EasyUIModel
+{
+   public class EasyUIDataGrid_Mission
+    {
+        public bool Checked { get; set; }
+
+        public int SMID { get; set; }
+
+        public string SMSMissionName { get; set; }
+
+        public string Remark { get; set; }
+
+        public int Sort { get; set; }
+
+        public bool selected { get; set; }
+    }
+}
+
