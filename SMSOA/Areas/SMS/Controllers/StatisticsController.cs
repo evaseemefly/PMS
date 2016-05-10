3cd2c19480f98755692e7d19a96aa9f12e975ee6
t430 <evaseemefly@126.com>
2016-05-10 20:48:37 +0800

    5月10日 家中晚上发送统计界面

--- SMSOA/Areas/SMS/Controllers/StatisticsController.cs
+++ SMSOA/Areas/SMS/Controllers/StatisticsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PMS.IBLL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,32 @@ using System.Web.Mvc;
 
 namespace SMSOA.Areas.SMS.Controllers
 {
-    public class StatisticsController : Controller
+    public class StatisticsController : Admin.Controllers.BaseController
     {
+        IUserInfoBLL userBLL { get; set; }
+
         // GET: SMS/Statistics
         public ActionResult Index()
         {
+            ViewBag.GetStatistic = "";
             return View();
+        }
+
+        /// <summary>
+        /// 查询当前用户的短信发送情况
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetStatistic_DataGrid()
+        {
+            //1 获取当前登录的用户
+            var userInfo = base.LoginUser;
+            var userTemp= userBLL.GetListBy(u => u.ID == userInfo.ID).FirstOrDefault();
+
+            //2 获取该用户所发送的短信内容——只获取当日的发送的短信（或发送最近的前十条短信）
+            var list_last10 = userTemp.S_SMSContent.OrderBy(c => c.SendDateTime).Take(10).ToList();
+
+
+
         }
     }
 }
