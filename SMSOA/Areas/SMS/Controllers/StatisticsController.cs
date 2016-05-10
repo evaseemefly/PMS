edc578cb8b0dd0bba46fcc28bad621326eee7304
liusihan-单位 <evaseemefly@126.com>
2016-05-10 16:41:55 +0800

    5月10日 单位完成统计添加母版页

---
+++ SMSOA/Areas/SMS/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSOA.Areas.SMS.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: SMS/Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
