c0027c2dc88792328c96a719d7be656489d98b75
t430 <evaseemefly@126.com>
2016-05-22 21:32:43 +0800

    5月22日 实现用户当日短信统计及最后10条短信统计显示功能

--- SMSOA/Areas/SMS/Controllers/StatisticsController.cs
+++ SMSOA/Areas/SMS/Controllers/StatisticsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SMSOA.Areas.SMS.Models;
+using PMS.Model;
 
 namespace SMSOA.Areas.SMS.Controllers
 {
@@ -16,7 +17,112 @@ namespace SMSOA.Areas.SMS.Controllers
         public ActionResult Index()
         {
             ViewBag.GetStatisticLast10 = "/SMS/Statistics/GetStatisticLast10_DataGrid";
+            ViewBag.GetStatisticLast10_Chart = "/SMS/Statistics/GetStatisticLast10_Chart";
+            ViewBag.GetStatisticCurrentDay_Chart = "/SMS/Statistics/GetStatisticCurrentDay_Chart";
+            ViewBag.GetStatisticCurrentDay = "/SMS/Statistics/GetStatisticCurrentDay_DataGrid";            
             return View();
+        }
+
+        private void GetStatistic_Model_Chart(int count,  List<ViewModel_StatisticsLast10> list_statistics,out ViewModel_Statistics_Chart viewModel_chart)
+        {
+            int[] array_total = new int[10];
+            int[] array_receive = new int[10];
+            DateTime[] array_datetime = new DateTime[10];
+            for (int i = 0; i < 10; i++)
+            {
+                if (list_statistics.Count > i)
+                {
+                    array_total[i] = list_statistics[i].TotalOfReceiveNum;
+                    array_receive[i] = list_statistics[i].TotalOfReceiveNum - list_statistics[i].NotReceiveNum;
+                    array_datetime[i] = list_statistics[i].SendDateTime;
+                }
+                else
+                {
+                    array_total[i] = 0;
+                    array_receive[i] = 0;
+                    array_datetime[i] = DateTime.Now;
+                }
+
+            }
+            //3 转成datagrid识别的json格式数据   
+            viewModel_chart = new ViewModel_Statistics_Chart()
+            {
+                Array_total = array_total,
+                Array_revice = array_receive,
+                Arrat_DataTime = array_datetime
+            };
+        }
+
+        private void GetStatisticList(List<S_SMSContent> listsource,ref List<ViewModel_StatisticsLast10> list_statistics)
+        {
+            //2.1 将短信内容实体对象集合转成要在datagrid中显示的统计对象集合
+            //2.2 需要统计该短信发送的人员个数以及未收到的人员个数
+            foreach (var item in listsource)
+            {
+                list_statistics.Add(
+                    new ViewModel_StatisticsLast10()
+                    {
+                        ContentID = item.ID,
+                        Content = item.SMSContent,
+                        MissionName = item.S_SMSMission.SMSMissionName,
+                        SendDateTime = item.SendDateTime,
+                        TotalOfReceiveNum = item.S_SMSRecord_Current.Count(),
+                        //找到未收到的（收到的状态码初步约定为100）
+                        NotReceiveNum = item.S_SMSRecord_Current.Where(r => r.ResultCode != 100).Count()
+                    });
+            }
+        }
+
+        /// <summary>
+        /// 获取当日的该用户的发送短信统计对象（使用图表的方式）
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetStatisticCurrentDay_Chart()
+        {
+            //1 获取当前登录的用户4
+            var userInfo = base.LoginUser;
+            var userTemp = userBLL.GetListBy(u => u.ID == userInfo.ID).FirstOrDefault();
+
+            //2 获取该用户所发送的短信内容——只获取当日的发送的短信
+            var currentDate = DateTime.Now.ToShortDateString();
+            var list_currentDay = userTemp.S_SMSContent.OrderBy(c => c.SendDateTime).Where(s => s.SendDateTime.ToShortDateString() == currentDate).ToList();
+
+            List<ViewModel_StatisticsLast10> list_statisticslast10 = new List<ViewModel_StatisticsLast10>();
+            //2.1 将短信内容实体对象集合转成要在datagrid中显示的统计对象集合
+            //2.2 需要统计该短信发送的人员个数以及未收到的人员个数
+            GetStatisticList(list_currentDay,ref list_statisticslast10);
+            var model = new ViewModel_Statistics_Chart();
+            GetStatistic_Model_Chart(10, list_statisticslast10,out model);
+
+            //4 序列化
+            return Content(Common.SerializerHelper.SerializerToString(model));
+
+        }
+
+        /// <summary>
+        /// 获取该用户的发送的最后10条短信统计对象（使用图表的方式）
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetStatisticLast10_Chart()
+        {
+            //1 获取当前登录的用户4
+            var userInfo = base.LoginUser;
+            var userTemp = userBLL.GetListBy(u => u.ID == userInfo.ID).FirstOrDefault();
+
+            //2 获取该用户所发送的短信内容——只获取发送最近的前十条短信
+            var list_last10 = userTemp.S_SMSContent.OrderBy(c => c.SendDateTime).Take(10).ToList();
+
+            List<ViewModel_StatisticsLast10> list_statisticslast10 = new List<ViewModel_StatisticsLast10>();
+            //2.1 将短信内容实体对象集合转成要在datagrid中显示的统计对象集合
+            //2.2 需要统计该短信发送的人员个数以及未收到的人员个数
+            GetStatisticList(list_last10, ref list_statisticslast10);
+
+            var model = new ViewModel_Statistics_Chart();
+            GetStatistic_Model_Chart(10, list_statisticslast10, out model);   
+
+            //4 序列化
+            return Content(Common.SerializerHelper.SerializerToString(model));
+
         }
 
         /// <summary>
@@ -35,20 +141,50 @@ namespace SMSOA.Areas.SMS.Controllers
             List<ViewModel_StatisticsLast10> list_statisticslast10 = new List<ViewModel_StatisticsLast10>();
             //2.1 将短信内容实体对象集合转成要在datagrid中显示的统计对象集合
             //2.2 需要统计该短信发送的人员个数以及未收到的人员个数
-            foreach (var item in list_last10)
+            //foreach (var item in list_last10)
+            //{
+            //    list_statisticslast10.Add(
+            //        new ViewModel_StatisticsLast10()
+            //        {
+            //            ContentID = item.ID,
+            //            Content = item.SMSContent,
+            //            MissionName = item.S_SMSMission.SMSMissionName,
+            //            SendDateTime = item.SendDateTime,
+            //            TotalOfReceiveNum = item.S_SMSRecord_Current.Count(),
+            //            //找到未收到的（收到的状态码初步约定为100）
+            //            NotReceiveNum = item.S_SMSRecord_Current.Where(r => r.ResultCode != 100).Count()
+            //        });
+            //}
+            GetStatisticList(list_last10, ref list_statisticslast10);
+
+
+            //3 转成datagrid识别的json格式数据            
+            PMS.Model.EasyUIModel.EasyUIDataGrid dgModel = new PMS.Model.EasyUIModel.EasyUIDataGrid()
             {
-                list_statisticslast10.Add(
-                    new ViewModel_StatisticsLast10()
-                    {
-                        ContentID = item.ID,
-                        Content = item.SMSContent,
-                        MissionName = item.S_SMSMission.SMSMissionName,
-                        SendDateTime = item.SendDateTime,
-                        TotalOfReceiveNum = item.S_SMSRecord_Current.Count(),
-                        //找到未收到的（收到的状态码初步约定为100）
-                        NotReceiveNum = item.S_SMSRecord_Current.Where(r => r.ResultCode != 100).Count()
-                    });
-            }
+                total = 0,
+                rows = list_statisticslast10,
+                footer = null
+            };
+            //4 序列化
+            return Content(Common.SerializerHelper.SerializerToString(dgModel));
+
+        }
+
+        public ActionResult GetStatisticCurrentDay_DataGrid()
+        {
+            //1 获取当前登录的用户
+            var userInfo = base.LoginUser;
+            var userTemp = userBLL.GetListBy(u => u.ID == userInfo.ID).FirstOrDefault();
+
+            //2 获取该用户所发送的短信内容——只获取当日的发送的短信（或发送最近的前十条短信）
+            //2 获取该用户所发送的短信内容——只获取当日的发送的短信
+            var currentDate = DateTime.Now.ToShortDateString();
+            var list_currentDay = userTemp.S_SMSContent.OrderBy(c => c.SendDateTime).Where(s => s.SendDateTime.ToShortDateString() == currentDate).ToList();
+
+            List<ViewModel_StatisticsLast10> list_statisticslast10 = new List<ViewModel_StatisticsLast10>();
+            //2.1 将短信内容实体对象集合转成要在datagrid中显示的统计对象集合
+            //2.2 需要统计该短信发送的人员个数以及未收到的人员个数
+            GetStatisticList(list_currentDay, ref list_statisticslast10);
 
 
             //3 转成datagrid识别的json格式数据            
---
+++ SMSOA/Areas/SMS/Models/ViewModel_Statistics_Chart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMSOA.Areas.SMS.Models
+{
+    public class ViewModel_Statistics_Chart
+    {
+        public int[] Array_total { get; set; }
+
+        public int[] Array_revice { get; set; }
+
+        public DateTime[] Arrat_DataTime { get; set; }
+    }
+}
