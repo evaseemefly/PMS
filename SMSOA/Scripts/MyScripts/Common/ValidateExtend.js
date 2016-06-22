806fd2505a94408f7107fe698b3ee9c47853ab2c
QuYuan891211 <quyuan891211@163.com>
2016-06-22 20:09:40 +0800

    6月22日 数据约束完成一半

--- SMSOA/Scripts/MyScripts/Common/ValidateExtend.js
+++ SMSOA/Scripts/MyScripts/Common/ValidateExtend.js
@@ -1,34 +1,34 @@
 ﻿//扩展easyui表单的验证
 $.extend($.fn.validatebox.defaults.rules, {
-    //验证汉字
+    //验证是否是汉字
     CHS: {
         validator: function (value) {
             return /^[\u0391-\uFFE5]+$/.test(value);
         },
-        message: 'The input Chinese characters only.'
+        message: '请输入中文'
     },
-    //移动手机号码验证
-    Mobile: {//value值为文本框中的值
+    //验证是否是合法的移动手机号码
+    Mobile: {
         validator: function (value) {
             var reg = /^1[3|4|5|8|9]\d{9}$/;
             return reg.test(value);
         },
-        message: 'Please enter your mobile phone number correct.'
+        message: '请输入合法的手机号码'
     },
-    //国内邮编验证
-    ZipCode: {
+
+    //验证是否是合法字符
+    String: {
         validator: function (value) {
-            var reg = /^[0-9]\d{5}$/;
-            return reg.test(value);
+            return /^[a-zA-Z0-9-\u0391-\uFFE5]+$/.test(value);
         },
-        message: 'The zip code must be 6 digits and 0 began.'
+        message:'不允许包含特殊符号'
     },
-    //数字
-    Number: {
-        validator: function (value) {
-            var reg = /^[0-9]*$/;
-            return reg.test(value);
+    //验证两次密码是否输入一致
+    Equals: {
+        validator: function (value, param) {
+            return value == $(param[0]).val();
         },
-        message: 'Please input number.'
-    },
+        message: '两次密码输入不一致'
+    }
+
 })
