6cee914f1d97a22879531d1c6ed693036c69bce4
liusihan-单位 <evaseemefly@126.com>
2016-06-24 17:12:53 +0800

    6月24日

---
+++ Common/RedisConfig.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ServiceStack.Redis;
+using ServiceStack.Text;
+using ServiceStack.Redis.Generic;
+
+using System.Configuration;
+
+namespace Common
+{
+   public sealed class RedisConfig:ConfigurationSection
+    {
+        public static RedisConfig GetConfig()
+        {
+            var section =(RedisConfig)ConfigurationManager.GetSection("RedisConfig");
+
+            return section;
+        }
+
+        public static RedisConfig GetConfig(string sectionName)
+        {
+            var section = (RedisConfig)ConfigurationManager.GetSection(sectionName);
+            if(section==null)
+            {
+                throw new ConfigurationErrorsException("Section " + sectionName + " is not found.");
+            }
+            return section;
+        }
+
+        [ConfigurationProperty]
+        public string WriteServerList
+        {
+            get
+            {
+                return (string)base["WriteServerList"];
+            }
+            set
+            {
+                base["WriteServerList"] = value;
+            }
+        }
+
+        public string ReadServerList
+        {
+            get
+            {
+                return (string)base["ReadServerList"];
+            }
+            set
+            {
+                base["ReadServerList"] = value;
+            }
+        }
+    }
+}
---
+++ SMSOA/obj/Debug/Package/PackageTmp/Scripts/MyScripts/EasyUI/SwitchBtn.js
@@ -0,0 +1,50 @@
+﻿function SetValCheckBySwitchBtn(id, temp) {
+    ///	<summary>
+    ///	为Switch按钮根据传入的temp设置value以及选中状态
+    ///
+    ///	</summary>
+    ///	<param name="id" type="string">
+    ///		id选择器要选择的id
+    ///	</param>
+    ///	<param name="temp" type="string">
+    ///		由后台传过来的true（注意此时为'True'，而非Bool类型
+    ///	</param>
+
+    var isShow;
+    var id_str = "#" + id;
+    //1 判断传入的temp是否为True，若为true则为isShow赋true
+    temp == 'True' ? isShow = true : isShow = false;
+
+
+    //2.1 若展示标记为true则为switch按钮的checked赋为选中
+    if (isShow == true) {
+        $(id_str).switchbutton({
+            checked: isShow
+        });
+
+    }
+    //2.2 位switch按钮的value赋为选中状态（bool）
+    $(id_str).switchbutton({
+        value: isShow
+    });
+}
+
+//为Switch按钮绑定切换为value属性赋值的事件
+function SwitchButtonBindSetValue(id) {
+    ///	<summary>
+    ///	为Switch按钮绑定切换为value属性赋值的事件
+    ///
+    ///	</summary>
+    ///	<param name="id" type="string">
+    ///		id选择器要选择的id
+    ///	</param>
+
+    var str = "#" + id;
+    $(str).switchbutton({
+        onChange: function (checked) {
+            $(str).switchbutton({
+                value: checked
+            });
+        }
+    });
+}
