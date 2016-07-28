003e8289b2c11c634a8591baba265ff1a8fe470c
HP840 <evaseemefly@126.com>
2016-07-28 10:06:57 +0800

    7月28日 单位拉去屈远之前的备份

--- PMS.BLL/S_SMSRecord_CurrentBLL.cs
+++ PMS.BLL/S_SMSRecord_CurrentBLL.cs
@@ -26,21 +26,29 @@ namespace PMS.BLL
                 var s_smsContent = this.CurrentDBSession.S_SMSContentDAL.GetListBy(r => r.ID == scid).FirstOrDefault();
                 //将长短信条数存入S_SMSContent
                 s_smsContent.smsCount = list_QueryReceive.FirstOrDefault().smsCount;
-
+                List<S_SMSRecord_Current> list_current = new List<S_SMSRecord_Current>();
                 //2. 遍历查询返回的集合
                 foreach (var item in list_QueryReceive)
                 {
                     //3.得到该条记录的电话号码对应的联系人
                     var person = this.CurrentDBSession.P_PersonInfoDAL.GetListBy(r => r.PhoneNum .Equals (item.phoneNumber)).FirstOrDefault();
-                    S_SMSRecord_Current smsRecord_Current = new S_SMSRecord_Current()
-                    {
-                        SCID = scid,             
-                        PID = person.PID,
-                        StatusCode = int.Parse(item.status),
-                        DescContent = item.desc,
-                    };
-                    this.CurrentDBSession.S_SMSRecord_CurrentDAL.Create(smsRecord_Current);
+                    //7月28日
+                    //思路：连接一次执行批量修改
+                    var record= this.CurrentDBSession.S_SMSRecord_CurrentDAL.GetListBy(r => r.PID == person.PID && r.SCID == scid).FirstOrDefault();
+                    record.StatusCode = int.Parse(item.status);
+                    record.DescContent = item.desc;
+                    list_current.Add(record);
+                    //S_SMSRecord_Current smsRecord_Current = new S_SMSRecord_Current()
+                    //{
+                    //    SCID = scid,             
+                    //    PID = person.PID,
+                    //    StatusCode = int.Parse(item.status),
+                    //    DescContent = item.desc,
+                    //};
+                    //this.CurrentDBSession.S_SMSRecord_CurrentDAL.Create(smsRecord_Current);
                 }
+                //批量更新
+                this.CurrentDBSession.S_SMSRecord_CurrentDAL.UpdateByList(list_current);
                 return this.CurrentDBSession.SaveChanges();
             }
             return false;
