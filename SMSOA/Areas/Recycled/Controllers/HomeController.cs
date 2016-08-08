0442cb6f0bb38eb24fbccb7b0a191011845b75b6
liusihan-单位 <evaseemefly@126.com>
2016-08-08 17:24:10 +0800

    8月8日单位

---
+++ SMSOA/Areas/Recycled/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSOA.Areas.Recycled.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Recycled/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
