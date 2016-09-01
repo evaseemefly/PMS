0e7eb29d26890c115e816ad25a5b1a9b3308c1cc
QuYuan891211 <quyuan891211@163.com>
2016-09-01 11:58:00 +0800

    9月1日 修改Bug 1.搜狗浏览器日期显示问题    2. 统计模块没有用的分页栏取消

---
+++ SMSOA/Scripts/MyScripts/Common/Overload_ToLocaleDateString.js
@@ -0,0 +1,8 @@
+﻿Date.prototype.toLocaleDateString = function (DatePattern) {
+    //重写toLocaleDateString方法，来固定输出格式，兼容多个浏览器
+    if (/yyyy.MM.dd/.test(DatePattern)) {
+        DatePattern = DatePattern.replace("yyyy", this.getFullYear()).replace("MM", this.getMonth() + 1).replace("dd", this.getDay());
+        return DatePattern;
+    }
+    return this.toLocaleString();
+};
