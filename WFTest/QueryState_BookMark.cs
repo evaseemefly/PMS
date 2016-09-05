dd0ee72505405ec13cd817c067da9deee90fc1c0
liusihan-单位 <evaseemefly@126.com>
2016-09-05 17:44:04 +0800

    9月5日 单位备份

---
+++ WFTest/QueryState_BookMark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace WFTest
+{
+
+    public sealed class QueryState_BookMark : NativeActivity
+    {
+        // 定义一个字符串类型的活动输入参数
+        public InArgument<string> Text { get; set; }
+
+        /// <summary>
+        /// 工作流名称
+        /// </summary>
+        public InArgument<string> BookMarkName { get; set; }
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            //1 从当前的上下文对象中获取指定名称的书签
+            string bookMarkName = context.GetValue(BookMarkName);
+            //2 创建书签
+            context.CreateBookmark(bookMarkName, new BookmarkCallback(ContinueExecuteWF));
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 恢复bookmark后调用的方法
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="bookmark"></param>
+        /// <param name="value"></param>
+        public void ContinueExecuteWF(
+    NativeActivityContext context,
+    Bookmark bookmark,object value)
+        {
+            //继续执行查询方法
+        }
+
+    }
+}
---
+++ WFTest/QueryStatesByMsgid_Code.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+using PMS.Model.SMSModel;
+
+namespace WFTest
+{
+
+    public sealed class QueryStatesByMsgid_Code : CodeActivity
+    {
+        // 定义一个字符串类型的活动输入参数E:\03协同开发\短信\PMS\0630\PMS\WFTest\QueryStatesByMsgid_Code.cs
+        public InArgument<string> Text { get; set; }
+
+            /// <summary>
+            /// 查询条件对象
+            /// </summary>
+        public InArgument<SMSModel_Query> query_model { get; set; }
+
+        //返回查询状态（先设定为string）类型
+        /// <summary>
+        /// 查询之后返回的状态
+        /// </summary>
+        public OutArgument<bool> State { get; set; }
+
+        // 如果活动返回值，则从 CodeActivity<TResult>
+        // 并从 Execute 方法返回该值。
+        protected override void Execute(CodeActivityContext context)
+        {
+            // 获取 Text 输入参数的运行时值
+            //string text = context.GetValue(this.Text);
+            
+            //进行查询传入的msgid的
+            
+        }
+    }
+}
