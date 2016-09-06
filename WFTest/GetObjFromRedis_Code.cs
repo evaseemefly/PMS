2c27927a41b1ebb5a7544606715259cdcb2ca6ea
HP840 <evaseemefly@126.com>
2016-09-06 16:57:34 +0800

    9月6日 单位完成部分

---
+++ WFTest/GetObjFromRedis_Code.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Activities;
+
+namespace WFTest
+{
+
+    public sealed class GetObjFromRedis_Code : CodeActivity
+    {
+        // 定义一个字符串类型的活动输入参数
+        public InArgument<string> Text { get; set; }
+        public InArgument<string> Id_List { get; set; }
+
+        public InArgument<double> secs_Interval { get; set; }
+
+
+        // 如果活动返回值，则从 CodeActivity<TResult>
+        // 并从 Execute 方法返回该值。
+        protected override void Execute(CodeActivityContext context)
+        {
+            // 获取 Text 输入参数的运行时值
+            string text = context.GetValue(this.Text);
+
+            //1 
+        }
+    }
+}
