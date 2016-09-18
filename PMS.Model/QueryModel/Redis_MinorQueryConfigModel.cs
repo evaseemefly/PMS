44f1e91c909c42332508ce3c63168b31e960e4e5
t430 <evaseemefly@126.com>
2016-09-18 18:51:34 +0800

    9月18日 家中备份

---
+++ PMS.Model/QueryModel/Redis_MinorQueryConfigModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Model.QueryModel
+{
+    public class Redis_MinorQueryConfigModel
+    {
+        /// <summary>
+        /// 第二线程读取对象判断的时间间隔
+        /// </summary>
+        public TimeSpan Interval_QueryAgain { get; set; }
+
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        public TimeSpan Interval_OverTime { get; set; }
+
+        /// <summary>
+        /// list的key
+        /// </summary>
+        public string List_Key { get; set; }
+
+        /// <summary>
+        /// hash的key
+        /// </summary>
+        public string Hash_Key { get; set; }
+
+    }
+}
