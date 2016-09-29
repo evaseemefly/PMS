084279675f198ebaa8c6f68f3da4b3687ae66adb
liusihan-单位 <evaseemefly@126.com>
2016-09-29 10:42:46 +0800

    9月29日 查询分配任务

---
+++ PMS.Model/ViewModel/ViewModel_ActionInfo_QueryInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Model.ViewModel
+{
+    /// <summary>
+    /// quyuan
+    /// </summary>
+   public class ViewModel_ActionInfo_QueryInfo
+    {
+        public string ActionInfoName { get; set; }
+
+        public string AreaName { get; set; }
+
+        public string ControllerName { get; set; }
+
+        public string ActionMethodName { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
---
+++ PMS.Model/ViewModel/ViewModel_UserInfo_QueryInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Model.ViewModel
+{
+    /// <summary>
+    /// quyuan
+    /// </summary>
+    public class ViewModel_UserInfo_QueryInfo
+    {
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UName { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+    }
+}
