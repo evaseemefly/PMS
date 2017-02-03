d9bda6314966d4cb542e1e767971c6b419e5a0e8
QuYuan891211 <quyuan891211@163.com>
2017-02-03 17:29:30 +0800

    彩信前台设计1（未完成）

---
+++ SMSOA/Areas/SMS/Controllers/MMSSendController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMSOA.Areas.SMS.Controllers
+{
+    public class MMSSendController : Controller
+    {
+        // GET: SMS/MMSSend
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
