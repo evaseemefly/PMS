ceadad19d2097a8c782e975450a30d7e0cc1c533
QuYuan891211 <quyuan891211@163.com>
2017-03-30 16:10:53 +0800

    I18N

--- SMSOA/Global.asax.cs
+++ SMSOA/Global.asax.cs
@@ -32,6 +32,29 @@ namespace SMSOA
             ThreadPool.QueueUserWorkItem((a)=>CallBack(), fileLogPath);  
         }
 
+        ///// <summary>
+        ///// 为国际化注册路由
+        ///// </summary>
+        ///// <param name="routes"></param>
+        //public static void RegisterRoutes(RouteCollection routes)
+        //{
+        //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        //    routes.MapRoute(
+        //        "Globalization", // 路由名称
+        //        "{lang}/{controller}/{action}/{id}", // 带有参数的 URL
+        //        new { lang = "zh", controller = "Home", action = "Index", id = UrlParameter.Optional }, // 参数默认值
+        //        new { lang = "^[a-zA-Z]{2}(-[a-zA-Z]{2})?$" }    //参数约束
+        //    );
+
+        //    routes.MapRoute(
+        //        "Default", // 路由名称
+        //        "{controller}/{action}/{id}", // 带有参数的 URL
+        //        new { controller = "Home", action = "Index", id = UrlParameter.Optional } // 参数默认值
+        //    );
+
+        //}
+
         private void CallBack()
         {
             while(true)
