4e930c53f5db52e39cc0ab284643b74c65ba3798
liusihan-单位 <evaseemefly@126.com>
2017-05-19 16:22:24 +0800

    5月19日 实现为用户设置默认点选的菜单

--- 相关文档/03SQL/查询用户的默认权限（显示）及修改.sql
+++ 相关文档/03SQL/查询用户的默认权限（显示）及修改.sql
@@ -7,4 +7,10 @@ where u.UName='admin' and a.ID=5
 --修改isDefault
 update R_UserInfo_ActionInfo
 set isDefault=1
-where id=14
+where id=24
+
+--查询指定用户所拥有的全部权限
+select r.* from dbo.R_UserInfo_ActionInfo as r
+left join dbo.UserInfo as u on r.UserInfoID=u.ID
+left join dbo.ActionInfo as a on r.ActionInfoID=a.ID
+where u.UName='admin'and a.ID=43
