5d45b3dd5cb2049ba703259dea6f6b17fcddd97b
liusihan-单位 <evaseemefly@126.com>
2017-08-31 14:49:22 +0800

    8月31日 单位提交

---
+++ PMS.Model/ApiMessage/SendResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMS.Model.ApiMessage
+{
+    /// <summary>
+    /// 执行发送api后返回给客户端的响应结果，可添加其他属性
+    /// </summary>
+    public class SendResponseModel
+    {
+        public string ResultCode { get; set; }
+
+        public DateTime ResponseDate { get; set; }
+
+
+    }
+}
